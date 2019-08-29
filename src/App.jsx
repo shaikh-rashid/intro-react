87816a2f5f3e2c8a2c323bc2eb2c3be8f8d07537
Shaikh Farhan Rashid <sk.f.rashid@gmail.com>
2019-08-28 23:49:48 -0400

    uncomment assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -49,16 +49,16 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
-      tabChoice = (
-        <List
-          placeholder="Add Assignment..."
-          currList={this.state.assignments}
-          addFunction={this.addAssignment}
-          title="Assignments"
-        />
-      );
-    }*/
+		if (this.state.buttonClicked === "assignments") {
+			tabChoice = (
+				<List
+					placeholder="Add Assignment..."
+					currList={this.state.assignments}
+					addFunction={this.addAssignment}
+					title="Assignments"
+				/>
+			);
+		}
 
     /* Change below to render students*/
 
